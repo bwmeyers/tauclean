2bdfd07a9156aafb759b3dc18abe6845cd5121e2
Bradley Meyers <bradley.meyers1993@gmail.com>
2019-03-27 15:32:00 +0800

    Fixed up x-unit calculations for simulating pulse profiles. Allowed users to define amplitude of components. Added a bit more verbosity.

--- simulate.py
+++ simulate.py
@@ -1,4 +1,5 @@
 import argparse
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 import pbf
@@ -9,43 +10,52 @@ from clean import gaussian
 np.random.seed(12345)
 
 
-def create_intrinsic_pulse(position, width, nbins=2048, dt=1.0):
-    x = dt * np.linspace(0, 1, nbins)
+def create_intrinsic_pulse(position, width, amps, nbins=2048, dt=1.0):
+    x = dt * np.linspace(0, nbins, nbins)
 
     f = np.zeros_like(x)
 
     # If a list of positions have been given, but only one value for the width, then create an array of widths that
     # are all the same
     if isinstance(position, list) and (not isinstance(width, list) or len(width) == 1):
+        print("Will use same width for each component")
         width = np.repeat(width, len(position))
 
+    if isinstance(position, list) and (not isinstance(amps, list) or len(amps) == 1):
+        print("Will use same amplitude for each component")
+        amps = np.repeat(amps, len(position))
+
     # For each position and width pair, add these components to the intrinsic pulse profile
-    for p, w in zip(position, width):
-        f += gaussian(x, p, w)
+    for p, w, a in zip(position, width, amps):
+        print("added gaussian comp.")
+        g = gaussian(x, p * dt, w * dt)
+        f += a * (g / g.max())
 
     # Normalise the output to have a maximum value of unity
-    f /= f.max()
+    f = f / f.max()
 
     return f
 
 
-def create_scattered_profile(intrinsic, tau, x0=0.0, pbftype="thin", dt=1.0, nrot=10, snr=500.0):
+def create_scattered_profile(intrinsic, tau, pbftype="thin", dt=1.0, nrot=10, snr=500.0):
     nbins = len(intrinsic)
 
-    x = dt * np.linspace(0, 1, nbins)
+    x = dt * np.linspace(0, nbins, nbins)
     pbf_x = dt * np.linspace(0, nrot, nrot * nbins)
 
     # Decide which PBF model to use
     if pbftype == "thin":
-        h = pbf.thin.thin_screen(pbf_x, tau, x0=x0)
+        h = pbf.thin(pbf_x, tau)
     elif pbftype == "thick":
-        h = pbf.thick(pbf_x, tau, x0=x0)
+        h = pbf.thick(pbf_x, tau)
     elif pbftype == "uniform":
-        h = pbf.uniform(pbf_x, tau, x0=x0)
+        h = pbf.uniform(pbf_x, tau)
     else:
         print("Invalid PBF type requested ({0})".format(pbftype))
         print("Defaulting to thin screen...")
-        h = pbf.thin(pbf_x, tau, x0=x0)
+        h = pbf.thin(pbf_x, tau)
+
+    h = np.roll(h, int(0.05 * nbins))
 
     # The observed pulse shape is the convolution of:
     # - the true signal,
@@ -53,7 +63,8 @@ def create_scattered_profile(intrinsic, tau, x0=0.0, pbftype="thin", dt=1.0, nro
     # - some Gaussian radiometer noise
     # Here we do the mode="full" convolution so that the complete shape is convolved and we don't end up with sharp edge
     # effects in the final profile that depend on where the shapes are defined (as in the case of mode="same")
-    scattered = np.convolve(intrinsic, h, mode="full") / np.sum(h)
+    scattered = np.convolve(intrinsic, h, mode="full")
+
     offset = np.argmax(intrinsic) - np.argmax(scattered)
     scattered = np.roll(scattered, offset)[:nrot*nbins]
 
@@ -73,45 +84,69 @@ def create_scattered_profile(intrinsic, tau, x0=0.0, pbftype="thin", dt=1.0, nro
     return h, scattered, observed
 
 
-def plot_simulated(intrinsic, kernel, scattered, observed, tau, pbftype, dt=1.0, phase=False, save=False):
+def plot_simulated(intrinsic, kernel, scattered, observed, tau, pbftype, snr, dt=1.0, xunit="time", save=False):
+
     nbins = len(intrinsic)
 
-    if phase:
+    if xunit == "phase":
         x = np.linspace(0, 1, nbins)
+        xlab = "Phase"
+    elif xunit == "time":
+        x = dt * np.linspace(0, nbins, nbins)
+        xlab = "Time (ms)"
+    elif xunit == "bins":
+        x = np.linspace(0, nbins, nbins).astype(int)
+        xlab = "Bins"
     else:
-        x = dt * np.linspace(0, 1, nbins)
+        print("Unknown x-unit: {0}".format(xunit))
+        sys.exit(1)
 
-    fig = plt.figure()
-    gs = fig.add_gridspec(2, 3, sharex=True)
+    fig = plt.figure(figsize=(10, 8))
+    gs = fig.add_gridspec(2, 3, hspace=0.3)
 
     ax_int = fig.add_subplot(gs[0, 0])
-    ax_ker = fig.add_subplot(gs[0, 1])
-    ax_sim = fig.add_subplot(gs[0, 2])
+    ax_ker = fig.add_subplot(gs[0, 1], sharex=ax_int)
+    ax_sim = fig.add_subplot(gs[0, 2], sharex=ax_int)
     ax_obs = fig.add_subplot(gs[1, :])
 
     ax_int.plot(x, intrinsic, color="C0")
     ax_int.set_title("Intrinsic pulse")
     ax_int.set_ylabel("Intensity")
+    ax_int.set_xlabel(xlab)
+    ax_int.set_xlim(0, x.max())
+    step = x.max() / 4.0
+    ax_int.set_xticks(np.arange(0, x.max() + step, step))
 
-    ax_ker.plot(x, kernel, color="C1")
+    ax_ker.plot(x, kernel, color="C1", label=r"$\rm \tau = {0:g} ms$".format(tau))
     ax_ker.set_title("Scattering kernel")
+    ax_ker.set_xlabel(xlab)
+    ax_ker.legend()
 
     ax_sim.plot(x, scattered, color="C2")
     ax_sim.set_title("Scattered profile")
+    ax_sim.set_xlabel(xlab)
 
     ax_obs.plot(x, observed, color="k")
-    ax_obs.set_title("Observed pulse profile (noise added)")
+    ax_obs.set_title(r"Observed pulse profile (noise added, $\rm SNR \approx {0}$)".format(snr))
     ax_obs.axhline(0, ls="--", color="r", lw=1)
     ax_obs.set_xlim(0, x.max())
-    if phase:
-        ax_obs.set_xlabel("Phase")
-    else:
-        ax_obs.set_xlabel("Time (ms)")
+    step = x.max() / 16.0
+    ax_obs.set_xticks(np.arange(0, x.max() + step, step))
+    ax_obs.grid(True)
+    ax_obs.set_xlabel(xlab)
 
     if save:
-        plt.show()
-    else:
         plt.savefig("simulated-profile_{0}-tau{1:g}.png".format(pbftype, tau), dpi=300, bbox_inches="tight")
+    else:
+        plt.show()
+
+
+def write_data(intrinsic, kernel, scattered, observed, pbftype, tau):
+
+    np.savetxt("sim-intrinsic_{0}-tau{1:g}.txt".format(pbftype, tau), intrinsic)
+    np.savetxt("sim-kernel_{0}-tau{1:g}.txt".format(pbftype, tau), kernel)
+    np.savetxt("sim-scattered_{0}-tau{1:g}.txt".format(pbftype, tau), scattered)
+    np.savetxt("sim-noisy_{0}-tau{1:g}.txt".format(pbftype, tau), observed)
 
 
 if __name__ == "__main__":
@@ -120,23 +155,33 @@ if __name__ == "__main__":
     parser.add_argument("-n", type=int, default=2048, help="number of profile bins")
     parser.add_argument("-p", nargs="+", type=int, help="centre positions of gaussian components (in bins)")
     parser.add_argument("-w", nargs="+", type=float, help="widths (std. dev.) of gaussian components (in bins)")
+    parser.add_argument("-a", nargs="+", type=float,
+                        help="amplitudes of gaussian components (will be normalised so that the maximum value is 1)")
     parser.add_argument("-k", default="thin",
                         help="PBF kernel type", choices=pbf.__all__)
     parser.add_argument("-t", type=float, default=0.05, help="scattering time scale (in ms)")
     parser.add_argument("--dt", type=float, default=1.0, help="time sample per bin (in ms)")
     parser.add_argument("-s", type=float, default=500.0, help="Desired signal-to-noise ratio")
-    parser.add_argument("--phase", action="store_true", default=False,
-                        help="Switch to plot in phase (turns) rather than time (ms)")
-    parser.add_argument("--save", action="store_true", default=False,
+    parser.add_argument("-x", default="bins", choices=["time", "phase", "bins"], help="plot x-axis units")
+    parser.add_argument("--saveplot", action="store_true", default=False,
                         help="Switch to save plot to disk rather than just show")
+    parser.add_argument("--write", action="store_true", default=False,
+                        help="Write the data (intrinsic, kernel, convolution and noisy) to files")
 
     args = parser.parse_args()
 
-    if len(args.w) > len(args.p):
-        print("Provided more widths that positions - truncating width array to match sizes")
-        args.w = args.w[:len(args.p) - 1]
+    if len(args.w) != len(args.p):
+        print("Provided different number of widths than positions, selecting first width")
+        args.w = args.w[0]
 
-    i = create_intrinsic_pulse(args.p, args.w, nbins=args.n, dt=args.dt)
-    k, s, o = create_scattered_profile(i, args.t, x0=0.1, pbftype=args.k, dt=args.dt, nrot=10, snr=args.s)
+    if len(args.a) != len(args.p):
+        print("Provided different number of amplitudes than positions, selecting first amplitude")
+        args.a = args.a[0]
 
-    plot_simulated(i, k, s, o, args.t, args.k, dt=args.dt, phase=args.phase, save=args.save)
+    i = create_intrinsic_pulse(args.p, args.w, args.a, nbins=args.n, dt=args.dt)
+    k, s, o = create_scattered_profile(i, args.t, pbftype=args.k, dt=args.dt, nrot=10, snr=args.s)
+
+    plot_simulated(i, k, s, o, args.t, args.k, snr=args.s, dt=args.dt, xunit=args.x, save=args.saveplot)
+
+    if args.write:
+        write_data(i, k, s, o, args.k, args.t)
