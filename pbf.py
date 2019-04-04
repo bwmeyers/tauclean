d9d6ef2b5b7da97b0de0d686771230558d651694
Bradley Meyers <bradley.meyers1993@gmail.com>
2019-04-04 10:06:42 +0800

    Set numpy warnings when evaluating PBFs as there will occasionally be division by zero and invalid float point operations as a consequence. This is resolved within the function, so the warning doesn't need to appear to the user.

--- pbf.py
+++ pbf.py
@@ -12,11 +12,11 @@ def thin(x, tau, x0=0):
     :param x0: where the PBF turns on [float, in range of x]
     :return: evaluated thin screen PBF [array-like]
     """
-    t = x
+    t = x - x0
     h = (1 / tau) * np.exp(-t / tau)  # normalised to unit area
 
-    # Turn on a unit step function at the given x0 offset.
-    h[x <= x0] = 0
+    # Turn on a unit step function at the given x0 offset, and turn nans into 0
+    h[np.where((x <= x0) & np.isnan(h))] = 0
 
     return h
 
@@ -30,12 +30,19 @@ def thick(x, tau, x0=0):
     :return: evaluated thick screen PBF [array-like]
     """
     t = x - x0
+
+    # ignore divide by zero and consequent invalid operation warnings due to very negative numbers (caused by providing
+    # large offsets through x0, size you can end up dividing by 0 or evaluating very large negative exponentials)
+    old_settings = np.seterr(divide='ignore', invalid='ignore')
+
     h = np.sqrt((np.pi * tau) / (4 * t ** 3)) * np.exp(-tau * np.pi ** 2 / (16 * t))  # normalised to unit area
+
+    np.seterr(**old_settings)  # restore old behaviour
 
     # In some cases (for negative t), h will not be well-defined (nan).
     # However, this is not important in this case, thus we can simply
     # replace those values with 0.
-    h[np.isnan(h)] = 0
+    h[np.where((x <= x0) & np.isnan(h))] = 0
 
     return h
 
@@ -49,11 +56,18 @@ def uniform(x, tau, x0=0):
     :return: evaluated PBF for a uniform scattering medium [array-like]
     """
     t = x - x0
+
+    # ignore divide by zero and consequent invalid operation warnings due to very negative numbers (caused by providing
+    # large offsets through x0, size you can end up dividing by 0 or evaluating very large negative exponentials)
+    old_settings = np.seterr(divide='ignore', invalid='ignore')
+
     h = np.sqrt((np.pi**5 * tau**3) / (8 * t**5)) * np.exp(-tau * np.pi**2 / (4 * t))  # normalised to unit area
+
+    np.seterr(**old_settings)  # restore old behaviour
 
     # In some cases (for negative t), h will not be well-defined (nan).
     # However, this is not important in this case, thus we can simply
     # replace those values with 0.
-    h[np.isnan(h)] = 0
+    h[np.where((x <= x0) & np.isnan(h))] = 0
 
     return h
