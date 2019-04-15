fda31566d8c019947e427ed12d18dae2beca059c
Bradley Meyers <bradley.meyers1993@gmail.com>
2019-04-14 17:57:21 -0700

    Fixed how f_r is calculated, returns NaN is all of the residuals are zero

--- tauclean/fom.py
+++ tauclean/fom.py
@@ -34,8 +34,8 @@ def positivity(res, off_rms, m=1.0, x=1.5):
     :param res: residuals after the CLEAN process has terminated [array-like]
     :param off_rms: off-pulse rms value to be used as a threshold [float]
     :param m: a scale-factor (or weight) that is of order unity [float]
-    :param x: threshold (units of off_rms) defined to penalise the positivity if
-    there residuals more negative than this [float]
+    :param x: threshold (units of off_rms) defined to penalise the positivity if there residuals more negative
+    than this [float]
     :return: the positivity figure of merit [float]
     """
 
@@ -43,7 +43,11 @@ def positivity(res, off_rms, m=1.0, x=1.5):
     # When the residual is less than x * rms, turn on the step-function.
     # This means that only those points that have been over subtracted (due to a poor choice in PBF)
     # contribute to this parameter.
-    u[res < x * off_rms] = 1
+    u[res < -x * off_rms] = 1
+
+    if np.all(res == 0):
+        # safe from a plotting perspective as NaNs are ignored
+        return np.nan
 
     f_r = (m / (len(res) * off_rms ** 2)) * np.sum(u * res ** 2)
 
