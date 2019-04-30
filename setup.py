ac35b973d6eda03aedb9a18fb3532b86b165f5ff
Bradley Meyers <bradley.meyers1993@gmail.com>
2019-04-30 10:08:04 +0800

    Update setup details

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from setuptools import setup
+from os import path
 import sys
+import re
 
 
 def get_version():
-    """Get the version number of tauclean"""
-    import tauclean
-    return tauclean.__version__
+    here = path.abspath(path.dirname(__file__))
+    with open(path.join(here, "tauclean/__init__.py")) as f:
+        contents = f.read()
+
+    return re.search(r"__version__ = \"(\S+)\"", contents).group(1)
 
 
-reqs = ['numpy>=1.16.2', 'matplotlib>=3.0.3', 'scipy>=1.2.1']
+reqs = ['numpy>=1.16.0', 'matplotlib>=3.0.0', 'scipy>=1.2.0']
 
 if sys.version_info < (3, 5):
     sys.exit('tauclean requires Python 3.5+')
@@ -19,7 +23,9 @@ setup(
     url='https://github.com/bwmeyers/tauclean',
     author='Bradley Meyers',
     author_email='bradley.meyers1993@gmail.com',
+    license='AFL-3.0',
     description='A package to deconvolve scattered pulsar profiles',
+    keywords="signal processing",
     install_requires=reqs,
     packages=['tauclean'],
     scripts=['scripts/tauclean', 'scripts/simulate'],
