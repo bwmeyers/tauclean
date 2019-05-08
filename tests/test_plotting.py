63aca40aceb69458a3202d1b030989d1a290d8b3
Bradley Meyers <bradley.meyers1993@gmail.com>
2019-05-08 09:52:01 +0800

    test that files written actually exist and contain the correct info. then remove intermediate files

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -8,10 +8,12 @@ Test plotting.py
 
 import os
 import pickle
+import glob
 
 import numpy as np
 
-from tauclean.plotting import plot_figures_of_merit, plot_clean_components, plot_clean_residuals, plot_reconstruction
+from tauclean.plotting import (plot_figures_of_merit, plot_clean_components, plot_clean_residuals,
+                               plot_reconstruction, write_output)
 
 np.random.seed(12345)
 TEST_DIR = '/'.join(os.path.realpath(__file__).split('/')[0:-1])
@@ -21,21 +23,60 @@ results = pickle.load(open("{TEST_DIR}/test_sample.p".format(TEST_DIR=TEST_DIR),
 results[-1]["fr"] = 100  # fake one fo the FoM to ensure that testing covers all code
 
 
+def remove_files(pattern):
+    flist = glob.glob("{0}/{1}".format(os.getcwd(), pattern))
+    for f in flist:
+        try:
+            os.remove(f)
+        except OSError:
+            raise AssertionError("error when deleting files")
+
+
 def test_plot_fom():
     if not plot_figures_of_merit(results):
         raise AssertionError()
+
+    remove_files("*.png")
 
 
 def test_plot_clean_residuals():
     if not plot_clean_residuals(init_data, results, period=500.0):
         raise AssertionError()
 
+    remove_files("*.png")
+
 
 def test_plot_clean_comps():
     if not plot_clean_components(results, period=500.0):
         raise AssertionError()
 
+    remove_files("*.png")
+
 
 def test_plot_reconstruction():
     if not plot_reconstruction(results, init_data, period=500.0):
         raise AssertionError()
+
+    remove_files("*.png")
+
+
+def test_write_output():
+    write_output(results)
+
+    # ensure the file was actually written
+    try:
+        lines = np.loadtxt("clean_components_thin-tau20.txt")
+    except FileNotFoundError:
+        raise AssertionError()
+
+    # tau = 20ms is item 2 in the results list
+    np.testing.assert_array_equal(lines, results[2]['cc'])
+
+    try:
+        lines = np.loadtxt("reconstruction_thin-tau20.txt")
+    except FileNotFoundError:
+        raise AssertionError()
+
+    np.testing.assert_array_equal(lines, results[2]['recon'])
+
+    remove_files("*.txt")
